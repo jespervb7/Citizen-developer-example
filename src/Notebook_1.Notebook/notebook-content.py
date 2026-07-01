105befdc1ad1d50d04dfe37706d8c13e9edd2c70
Jesper van Beemdelust <77440636+jespervb7@users.noreply.github.com>
2026-07-01 22:46:28 +0200

    Committing 3 items from workspace 1080b66a-5d2d-4549-90a8-f1dd1f4bd1e4

---
+++ src/Notebook_1.Notebook/notebook-content.py
@@ -0,0 +1,33 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "warehouse": {
+# META       "default_warehouse": "9076ce03-729f-ba64-4ee2-ba3709c773e5",
+# META       "known_warehouses": [
+# META         {
+# META           "id": "9076ce03-729f-ba64-4ee2-ba3709c773e5",
+# META           "type": "Datawarehouse"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
